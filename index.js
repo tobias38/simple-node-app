ca9411168fbcafb8cace38cbfcc901ee715c2c88
Tobias Stolzenberger <tobias.stolzenberger@sap.com>
2021-11-25 18:06:41 +0100

    Implemented proper shutdown hooks

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
+function shutdown() {
+  server.close(err => {
+    if (err) {
+      console.error(err);
+      process.exitCode = 1;
+    }
+    process.exit();
+  })
+}
+
